8814b3cc8fd3d5b36a10dc1259b2a7658bc57e5f
shubhams5 <suryawanshi.shubham50@gmail.com>
2020-12-30 17:36:31 +0530

    Frontend Changes

--- app.js
+++ app.js
@@ -199,5 +199,6 @@ app.post("/delete/:id", function (req, res) {
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
+  console.log("Server started on port 3000");
 }
 app.listen(port);
